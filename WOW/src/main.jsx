05b0f59d6e89d15688cb96209737ba79aa54eef7
bettercodevivek <viveksingh0520@gmail.com>
2024-08-10 21:51:49 +0530

    Updation

--- WOW/src/main.jsx
+++ WOW/src/main.jsx
@@ -1,10 +1,45 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx';
 import './index.css'
+
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+        {
+          index:true,
+          element:<Home/>
+        },
+        {
+          path:'home',
+          element:<Home/>
+        },
+        {
+          path:'vision',
+          element:<Vision/>
+        },
+        {
+          path:'profile',
+          element:<Profile/>
+        },{
+          path:'products',
+          element:<Products/>
+        },
+        {
+          path:'contact',
+          element:<Contact/>
+        }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={router}/>
   </StrictMode>,
 )
