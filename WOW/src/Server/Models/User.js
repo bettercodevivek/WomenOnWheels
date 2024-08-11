f923008a690accb31451b7ab36d762c968bc3b08
bettercodevivek <viveksingh0520@gmail.com>
2024-08-11 16:09:31 +0530

    Updation

---
+++ WOW/src/Server/Models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    mobileNumber: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true }
+});
+
+module.exports = mongoose.model('User', userSchema);
